ebd313569c498695b7093e3e4ee62fe8e5f265ea
Hans Preinfalk <hans.preinfalk.davila@gmail.com>
2026-05-06 07:44:29 -0700

    Initial Commit

---
+++ calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by Hans Preinfalk on 5/6/26.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
